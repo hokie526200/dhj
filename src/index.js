ab5f225cf8f5f4fa3921ca01aa329c205f1fb4ee
hokie <1582644959@qq.com>
2023-02-04 14:47:14 +0800

    修改了bug

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { HashRouter } from "react-router-dom"
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { createTheme, ThemeProvider } from '@mui/material/styles'
+
+import "./assets/css/index.css"
+// import 'antd/dist/antd.css'
+import 'antd/dist/antd.min.css'
+import "normalize.css"
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#484848"
+    }
+  }
+})
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  // <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <HashRouter><App/></HashRouter>
+    </ThemeProvider>
+  // </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
